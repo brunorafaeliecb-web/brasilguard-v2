7788bfca3ceced9d0a470e597b8bf3e4414a3dee
Bruno Rafael Silva Pok <brunorafael.iecb@gmail.com>
2026-09-04 08:00:00 -0300

    chore(agenda): add profile API endpoint for v0003.a

--- modules/bgd-agenda-firefox/config.js
+++ modules/bgd-agenda-firefox/config.js
@@ -6,6 +6,7 @@ const BGD_CONFIG = {
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_O-JhQJkS7jmHwPn7qYExZg_pjIyIZ-N",
   BACKEND_URL: "https://zwtmcqtepkzxuhilxyjx.supabase.co/functions/v1/bgd-agenda-appointments",
   PLATFORM_API_URL: "https://zwtmcqtepkzxuhilxyjx.supabase.co/functions/v1/bgd-agenda-platform-v3",
+  PROFILE_API_URL: "https://zwtmcqtepkzxuhilxyjx.supabase.co/functions/v1/bgd-agenda-profile-v3",
   DEFAULT_TENANT_SLUG: "brasilguard-default",
   API_VERSION: "v1",
   GOOGLE_CLIENT_ID: "752721916663-l59ed4t5h8bcts9b0pojk4ed7aca5l5n.apps.googleusercontent.com",
